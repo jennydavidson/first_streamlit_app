adc2ecf06e85f3797db159b4f2f92b8ba7a79893
jennydavidson <126829579+jennydavidson@users.noreply.github.com>
2023-03-02 23:16:00 +0000

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mum''s New Healthy Diner')
+streamlit.title('My Mum\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣  Omega 3 & blueberry oatmeal')
