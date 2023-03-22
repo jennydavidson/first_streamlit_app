8b6827ed9135da21a6d75ccdb2d4a0eeed950a85
jennydavidson <126829579+jennydavidson@users.noreply.github.com>
2023-03-22 23:04:55 +0000

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -49,20 +49,24 @@ streamlit.write('The user entered ', fruit_choice)
 # write your own comment - reads into a normalised variable
 # write your own comment - puts this into a table
 
+streamlit.header("The fruit load list contains")
+def get_fruit_load_list:
+    with  my_cur = my_cnx.cursor() as my_cur:
+          my_cur.execute("select * from fruit_load_list")
+          return my_cur.fetchall()
+    
+    if streamlit.button('Get Fruit Load List'):
+        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        my_data_rows = get_fruit_load_list()
+        streamlit.dataframe(my_data_rows)
+          
+    
+    
 
 streamlit.stop()
 
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_rows)
-
-
 my_fruit_add = streamlit.text_input('What fruit would you like to add?','')
 streamlit.write('You Chose ', my_fruit_add)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
