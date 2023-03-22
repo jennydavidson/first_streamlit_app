c608b2c2d4f09f91201d0b0a52d7784122d0fb29
jennydavidson <126829579+jennydavidson@users.noreply.github.com>
2023-03-22 23:24:17 +0000

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -59,11 +59,7 @@ def get_fruit_load_list():
         my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
         my_data_rows = get_fruit_load_list()
         streamlit.dataframe(my_data_rows)
-          
-    
-    
-
-
+  
 
 def insert_new_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
@@ -76,4 +72,11 @@ if  streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_new_row_snowflake(my_fruit_add)
     streamlit.text(back_from_function)
+    
+    if streamlit.button('Get Fruit Load List'):
+        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        my_data_rows = get_fruit_load_list()
+        my_cnx.close()
+        streamlit.dataframe(my_data_rows)
+          
 
